62e3b295d2b2f26c2f3ca40478fd5c06db2adf0c
John ‏‏‎ <john@moebius.ph>
2020-06-30 16:50:25 +0800

    finished coding DesktopSingle design

--- src/components/views/Singular.vue
+++ src/components/views/Singular.vue
@@ -52,9 +52,9 @@
     </div>
     <div class="w-full mt-8 px-4 bg-white">
       <div
-        class="container border border-solid rounded border-flgray-normal box-border mb-4"
+        class="container border border-solid rounded border-flgray-normal box-border mb-4 mx-auto"
       >
-        <div class="site-logo bg-flblue-light py-4">
+        <div class="site-logo bg-flblue-light py-4 lg:hidden">
           <img
             class="w-2/3 mx-auto"
             src="../../assets/images/friends-with-benefits.png"
@@ -63,90 +63,142 @@
         </div>
         <div class="site-img border border-solid border-flgray-normal">
           <img
-            class="w-full"
+            class="w-full lg:mt-12"
             src="../../assets/images/friend-with-benefits-single.jpg"
             alt="friend-with-benefits-single"
           />
         </div>
-        <div class="table-header grid grid-cols-2 bg-flblue-light text-center">
-          <div class="py-5">
-            <h3 class="text-white uppercase text-base font-bold tracking-tight">
-              rating
-            </h3>
+        <div class="dt-table flex">
+          <div
+            class="tb-site-logo bg-flblue-light py-4 hidden lg:flex w-2/5 items-center border-r border-solid border-flgray-normal box-border px-10"
+          >
+            <img
+              src="../../assets/images/friends-with-benefits.png"
+              alt="friends-with-benefits"
+            />
           </div>
-          <div class="py-5">
-            <h3 class="text-white uppercase text-base font-bold tracking-tight">
-              popularity
-            </h3>
-          </div>
-        </div>
-        <div class="table-contents">
-          <ul>
-            <li class="grid grid-cols-2">
-              <div class="flex-initial flex items-center justify-around">
-                <span
-                  class="block text-3xl font-bold tracking-tight text-flgray-dark"
-                  >8.9</span
+          <div class="site-stats flex-1">
+            <div
+              class="table-header grid grid-cols-2 lg:grid-cols-3 bg-flblue-light text-center"
+            >
+              <div class="py-5">
+                <h3
+                  class="text-white uppercase text-base font-bold tracking-tight"
                 >
-                <img
-                  class="flex-initial"
-                  src="../../assets/images/top-heart-icon.png"
-                  alt="top-heart-icon"
-                />
+                  rating
+                </h3>
               </div>
-              <div
-                class="flex flex-col justify-center px-2 py-3 border border-t-0 border-r-0 border-b-0 border-solid border-flgray-normal bg-white lg:px-12 lg:py-6"
-              >
-                <div
-                  class="flex items-center justify-center border border-solid border-flblue-light rounded text-center px-5 py-1 lg:p-2"
+              <div class="hidden lg:block py-5">
+                <h3
+                  class="text-white uppercase text-base font-bold tracking-tight"
                 >
-                  <a class="inline-block lg:hidden" href="#">
-                    <img
-                      class="w-5"
-                      src="../../assets/images/topup.png"
-                      alt="topup"
-                    />
-                  </a>
-                  <div class="inline-block mx-1">
+                  members
+                </h3>
+              </div>
+              <div class="py-5">
+                <h3
+                  class="text-white uppercase text-base font-bold tracking-tight"
+                >
+                  popularity
+                </h3>
+              </div>
+            </div>
+            <div class="table-contents">
+              <ul>
+                <li class="grid grid-cols-2 lg:grid-cols-3">
+                  <div class="flex-initial flex items-center justify-evenly">
                     <span
-                      class="text-flblue-mdark font-bold text-sm tracking-tight lg:text-lg"
-                      >+1714</span
+                      class="block text-3xl font-bold tracking-tight text-flgray-dark"
+                      >8.9</span
+                    >
+                    <img
+                      class="flex-initial"
+                      src="../../assets/images/top-heart-icon.png"
+                      alt="top-heart-icon"
+                    />
+                  </div>
+                  <div
+                    class="flex-col justify-center py-10 border-l border-solid border-flgray-normal bg-white hidden lg:flex"
+                  >
+                    <div
+                      class="flex-initial flex items-center justify-center mb-6"
+                    >
+                      <img
+                        class="flex-initial"
+                        src="../../assets/images/personicontwo.png"
+                        alt="personicontwo"
+                      />
+                    </div>
+                    <div
+                      class="flex-initial flex items-center justify-center mb-4"
+                    >
+                      <span
+                        class="text-center text-lg font-bold text-black tracking-tight"
+                        >2.500.00</span
+                      >
+                    </div>
+                    <div class="flex-initial flex items-center justify-center">
+                      <span
+                        class="text-center text-sm font-medium text-flblue-light tracking-tight leading-tight"
+                        >51% women</span
+                      >
+                    </div>
+                  </div>
+                  <div
+                    class="flex flex-col justify-center px-2 py-3 border border-t-0 border-r-0 border-b-0 border-solid border-flgray-normal bg-white lg:px-12 lg:py-6"
+                  >
+                    <div
+                      class="flex items-center justify-center border border-solid border-flblue-light rounded text-center px-5 py-1 lg:p-2"
+                    >
+                      <a class="inline-block lg:hidden" href="#">
+                        <img
+                          class="w-5"
+                          src="../../assets/images/topup.png"
+                          alt="topup"
+                        />
+                      </a>
+                      <div class="inline-block mx-1">
+                        <span
+                          class="text-flblue-mdark font-bold text-sm tracking-tight lg:text-lg"
+                          >+1714</span
+                        >
+                      </div>
+                      <a class="inline-block lg:hidden" href="#">
+                        <img
+                          class="w-5"
+                          src="../../assets/images/topdown.png"
+                          alt="topdown"
+                        />
+                      </a>
+                    </div>
+                    <div class="text-center hidden px-8 py-3 lg:block lg:p-2">
+                      <a class="lg:inline-block hidden mr-3" href="#">
+                        <img
+                          class="w-7"
+                          src="../../assets/images/topup.png"
+                          alt="topup"
+                        />
+                      </a>
+                      <a class="lg:inline-block hidden ml-3" href="#">
+                        <img
+                          class="w-7"
+                          src="../../assets/images/topdown.png"
+                          alt="topdown"
+                        />
+                      </a>
+                    </div>
+                    <a
+                      class="block rounded bg-florange-normal text-white text-center py-1 mt-3 lg:py-2 lg:mt-0"
+                      href="#"
+                      >Visit web</a
                     >
                   </div>
-                  <a class="inline-block lg:hidden" href="#">
-                    <img
-                      class="w-5"
-                      src="../../assets/images/topdown.png"
-                      alt="topdown"
-                    />
-                  </a>
-                </div>
-                <div class="text-center hidden px-8 py-3 lg:p-2">
-                  <a class="lg:inline-block hidden mr-3" href="#">
-                    <img
-                      class="w-7"
-                      src="../../assets/images/topup.png"
-                      alt="topup"
-                    />
-                  </a>
-                  <a class="lg:inline-block hidden ml-3" href="#">
-                    <img
-                      class="w-7"
-                      src="../../assets/images/topdown.png"
-                      alt="topdown"
-                    />
-                  </a>
-                </div>
-                <a
-                  class="block rounded bg-florange-normal text-white text-center py-1 mt-3"
-                  href="#"
-                  >Visit web</a
-                >
-              </div>
-            </li>
-          </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div class="desc border-t border-flgray-normal p-4">
+        <div class="desc border-t border-flgray-normal p-4 lg:p-10">
           <div>
             <h3
               class="text-flblue-light font-bold text-base leading-loose tracking-tight lg:mt-4"
@@ -165,6 +217,12 @@
               a tablet; a massive advantage if you happen to be away from your
               laptop.
             </p>
+            <br />
+            <a
+              class="hidden lg:block w-2/3 mx-auto rounded bg-florange-normal text-white text-center py-2 mt-3 lg:w-1/6"
+              href="#"
+              >Visit web</a
+            >
             <br />
             <h3
               class="text-flblue-light font-bold text-base leading-loose tracking-tight"
@@ -203,7 +261,7 @@
             </p>
             <br />
             <a
-              class="block w-2/3 mx-auto rounded bg-florange-normal text-white text-center py-2 mt-3"
+              class="block w-2/3 mx-auto rounded bg-florange-normal text-white text-center py-2 mt-3 lg:w-1/6"
               href="#"
               >Visit web</a
             >
@@ -211,24 +269,26 @@
         </div>
       </div>
       <div
-        class="container border border-solid rounded border-flgray-normal box-border"
+        class="container border border-solid rounded border-flgray-normal box-border mx-auto"
       >
         <div
           class="py-5 px-8 border-b border-solid border-flgray-normal box-border"
         >
-          <h3 class="text-flblue-light font-bold text-base tracking-tight">
+          <h3
+            class="text-flblue-light font-bold text-base tracking-tight uppercase"
+          >
             Opiniones
           </h3>
         </div>
         <ul>
           <li class="border-b border-solid border-flgray-normal box-border">
-            <div class="p-4 clearfix">
+            <div class="p-4 lg:p-8 clearfix">
               <span
                 class="float-right text-flblue-light font-bold text-base leading-loose tracking-tight"
                 >#1</span
               >
               <img
-                class="w-1/3"
+                class="w-1/3 lg:w-1/12"
                 src="../../assets/images/profile-pic.png"
                 alt="profile-pic"
               />
@@ -262,13 +322,55 @@
             </div>
           </li>
           <li class="border-b border-solid border-flgray-normal box-border">
-            <div class="p-4 clearfix">
+            <div class="p-4 lg:p-8 clearfix">
               <span
                 class="float-right text-flblue-light font-bold text-base leading-loose tracking-tight"
                 >#2</span
               >
               <img
-                class="w-1/3"
+                class="w-1/3 lg:w-1/12"
+                src="../../assets/images/profile-pic.png"
+                alt="profile-pic"
+              />
+              <h3
+                class="text-flblue-light font-bold text-base leading-loose tracking-tight"
+              >
+                Olivia Arribas
+              </h3>
+              <br />
+              <p
+                class="text-black text-base font-normal leading-loose tracking-tight"
+              >
+                We analyze objectively and for free, the best English-speaking
+                websites to help those who want to venture into the dating
+                online world but unfortunately do not know where to start.<br />
+                Do not hesitate, follow our advice to find the best social
+                network that best suit what you’re looking for, Joining the most
+                effective way to find relationships!
+              </p>
+              <br />
+              <a
+                class="inline-block text-flblue-light font-bold text-base leading-loose tracking-tight uppercase float-right"
+                href="#"
+                ><img
+                  class="inline-block w-2/12"
+                  src="../../assets/images/blue-arrow-left.png"
+                  alt="blue-arrow-left"
+                />
+                <span class="inline-block ml-2">responder</span></a
+              >
+            </div>
+          </li>
+          <li
+            class="hidden lg:block border-b border-solid border-flgray-normal box-border"
+          >
+            <div class="p-4 lg:p-8 clearfix">
+              <span
+                class="float-right text-flblue-light font-bold text-base leading-loose tracking-tight"
+                >#3</span
+              >
+              <img
+                class="w-1/3 lg:w-1/12"
                 src="../../assets/images/profile-pic.png"
                 alt="profile-pic"
               />
@@ -302,7 +404,7 @@
             </div>
           </li>
         </ul>
-        <div class="p-4">
+        <div class="p-4 lg:p-8">
           <h3
             class="text-flblue-light font-bold text-base leading-loose tracking-tight uppercase"
           >
@@ -315,32 +417,32 @@
             Tu dirección de correo electrónico no serä publicada. Los campos
             obligatorios estän marcados con *
           </p>
-          <form action="#">
+          <form action="#" class="lg:grid lg:grid-cols-4 lg:gap-3 lg:row-gap-4">
             <textarea
               name="comments"
               id="comments"
               rows="5"
               placeholder="Tu opinión"
-              class="w-full border border-solid rounded border-flgray-normal box-border p-4 mb-4 placeholder-flgray-normal"
+              class="w-full border border-solid rounded border-flgray-normal box-border p-4 mb-4 placeholder-flgray-normal lg:col-span-4 lg:mb-0"
             ></textarea>
             <input
               type="text"
               placeholder="Nombre*"
-              class="w-full border border-solid rounded border-flgray-normal box-border px-4 py-3 mb-4 placeholder-flgray-normal"
+              class="w-full border border-solid rounded border-flgray-normal box-border px-4 py-3 mb-4 placeholder-flgray-normal lg:mb-0"
             />
             <input
               type="email"
               placeholder="Email*"
-              class="w-full border border-solid rounded border-flgray-normal box-border px-4 py-3 mb-4 placeholder-flgray-normal"
+              class="w-full border border-solid rounded border-flgray-normal box-border px-4 py-3 mb-4 placeholder-flgray-normal lg:mb-0"
             />
             <input
               type="url"
               placeholder="Web"
-              class="w-full border border-solid rounded border-flgray-normal box-border px-4 py-3 mb-4 placeholder-flgray-normal"
+              class="w-full border border-solid rounded border-flgray-normal box-border px-4 py-3 mb-4 placeholder-flgray-normal lg:mb-0"
             />
             <button
               type="submit"
-              class="w-full rounded bg-flblue-light text-white font-bold text-base text-center tracking-tight uppercase py-2"
+              class="w-full rounded bg-flblue-light text-white font-bold text-base text-center tracking-tight uppercase py-2 lg:mb-0"
             >
               <img
                 class="w-1/12 inline-block -mt-1"
